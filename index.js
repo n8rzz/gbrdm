bb243bc480a23bd04706389470ce64f03ddc1e0d
Nate Geslin <ngeslin@nerdery.com>
2017-12-09 22:45:21 -0600

    adds logic to extract removable git branch names from current working directory

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
 const { exec } = require('child_process');
 const chalk = require('chalk');
-
-const WHITESPACE_REGEX = / /g;
-const NEWLINE_CHARACTER = '\n';
-
-function _extractBranchListFromStr(str) {
-    const branchList = str.split(NEWLINE_CHARACTER);
-    const trimmedBranchList = branchList.reduce((sum, branch) => {
-        if (branch !== '') {
-            sum.push(branch.replace(WHITESPACE_REGEX, ''));
-        }
-
-        return sum;
-    }, []);
-
-    return trimmedBranchList;
-};
+const extractBranchList = require('./lib/extractBranchList');
 
 exec('git branch', (err, stdout, stderr) => {
     if (err) {
         throw err;
     }
 
-    const branchList = _extractBranchListFromStr(stdout);
+    const branchList = extractBranchList(stdout);
     console.log('+++', branchList);
 });
 
