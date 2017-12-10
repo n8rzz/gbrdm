51be6352cd4803ad2afa6021cc684a185cc064d8
Nate Geslin <ngeslin@nerdery.com>
2017-12-09 23:19:38 -0600

    completes functionality

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { exec } = require('child_process');
+const { prompt } = require('inquirer');
 const chalk = require('chalk');
 const extractBranchList = require('./lib/extractBranchList');
 
@@ -7,7 +8,72 @@ exec('git branch', (err, stdout, stderr) => {
         throw err;
     }
 
-    const branchList = extractBranchList(stdout);
-    console.log('+++', branchList);
+    extractBranchNames(stdout);
 });
 
+function extractBranchNames(stdout) {
+    const branchList = extractBranchList(stdout);
+    console.log('+++', branchList);
+
+    if (branchList.length === 0) {
+        console.log(chalk.red('No non-reserved branches were found to remove.'));
+
+        return;
+    }
+
+    createCheckboxListForBranchList(branchList);
+}
+
+function createCheckboxListForBranchList(branchList) {
+    const questions = [
+        {
+            name: 'branchesToDelete',
+            type: 'checkbox',
+            message: 'Select the branches you would like to delete (use the spacebar to select)',
+            choices: branchList
+        }
+    ];
+
+    prompt(questions)
+        .then((answers) => {
+            confirmBranchesToDelete(answers.branchesToDelete);
+        });
+}
+
+function confirmBranchesToDelete(branchList) {
+    prompt({
+        name: 'confirm',
+        type: 'confirm',
+        message: `Are you sure you want to delete these branches: ${branchList.join(', ')}?`
+    }).then((answers) => {
+        if (!answers.confirm) {
+            console.log(chalk.red('Aborting delete.  Whew!'));
+
+            return;
+        }
+
+        _deleteBranchList(branchList);
+    });
+}
+
+function _deleteBranchList(branchList) {
+    for (let i = 0; i < branchList.length; i++) {
+        const branchName = branchList[i];
+
+        try {
+            _deleteGitBranch(branchName);
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+function _deleteGitBranch(branchName) {
+    exec(`git branch -D ${branchName}`, (err, stdout, stderr) => {
+        if (err) {
+            throw err;
+        }
+
+        console.log(chalk.yellow(`Deleted branch: ${branchName}`));
+    });
+}
