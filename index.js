cbbc298b3e7dec64a526f299a14337b8761e3fd1
Nate Geslin <ngeslin@nerdery.com>
2018-06-03 15:37:39 -0500

    implements `@n8rzz/branches` and removes git branch logics * restructures app file layout in favor of single file

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-require('./lib/gbdm');
+const { exec } = require('child_process');
+const { prompt } = require('inquirer');
+const chalk = require('chalk');
+const branches = require('@n8rzz/branches');
+
+function _buildCheckboxList(branchList) {
+    const questions = [
+        {
+            name: 'branchesToDelete',
+            type: 'checkbox',
+            message: 'Select branches to delete',
+            choices: branchList
+        }
+    ];
+
+    prompt(questions)
+        .then((answers) => {
+            _confirmBranchesToDelete(answers.branchesToDelete);
+        });
+}
+
+function _confirmBranchesToDelete(branchList) {
+    prompt({
+        name: 'confirm',
+        type: 'confirm',
+        message: `Are you sure you want to delete these branches: ${branchList.join(', ')}?`
+    }).then((answers) => {
+        if (!answers.confirm) {
+            console.log(chalk.red('Aborting delete.  Whew!'));
+
+            return;
+        }
+
+        _deleteBranchList(branchList);
+    });
+}
+
+function _deleteBranchList(branchList) {
+    for (let i = 0; i < branchList.length; i++) {
+        const branchName = branchList[i];
+
+        try {
+            _deleteSingleBranch(branchName);
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+function _deleteSingleBranch(branchName) {
+    exec(`git branch -D ${branchName}`, (err, stdout, stderr) => {
+        if (err) {
+            throw err;
+        }
+
+        console.log(chalk.yellow(`Deleted branch: ${branchName}`));
+    });
+}
+
+
+(function () {
+    return branches().then((branchList) => _buildCheckboxList(branchList));
+})();
